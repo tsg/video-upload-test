cd70d0e8fc73410a292ddc506834a3e928351d1b
Tudor Golubenco <tudor@xata.io>
2023-10-10 19:25:45 +0200

    Connect to my db + vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import vercelServerless from "@astrojs/vercel/serverless";
+import tailwind from "@astrojs/tailwind";
 
-import tailwind from '@astrojs/tailwind';
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server',
+  output: "server",
+  adapter: vercel(),
 });
